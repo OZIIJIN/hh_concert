7bb08c3df1407acd92306a1c8cf1be75872b7bf3
OZIIJIN <dpwlsdlqma6@gmail.com>
2025-04-10 03:41:01 +0900

    #2 feat : ConcertResponse 추가

---
+++ src/main/java/kr/hhplus/be/server/presentation/ConcertResponse.java
@@ -0,0 +1,35 @@
+package kr.hhplus.be.server.presentation;
+
+import kr.hhplus.be.server.domain.Concert;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ConcertResponse {
+
+    public record GetConcertDetail(
+            Long concertId,
+            String concertName,
+            String concertLocation,
+            LocalDate concertStartDate,
+            LocalDate concertEndDate
+    ) {
+        public static GetConcertDetail from(Concert concert) {
+            return new GetConcertDetail(
+                    concert.getId(),
+                    concert.getName(),
+                    concert.getLocation(),
+                    concert.getStartDate(),
+                    concert.getEndDate());
+        }
+    }
+
+    public record GetConcertList(
+            List<GetConcertDetail> concerts
+    ) {
+        public static GetConcertList from(List<GetConcertDetail> concerts) {
+            return new GetConcertList(concerts);
+        }
+    }
+}
