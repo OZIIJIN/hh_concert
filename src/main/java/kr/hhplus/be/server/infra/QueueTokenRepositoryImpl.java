b72d7fb26a98ff7cf24dfd3771fbd5c079441a07
OZIIJIN <dpwlsdlqma6@gmail.com>
2025-04-20 17:37:00 +0900

    #9 feat: QueueTokenRepositoryImpl 추가

--- src/main/java/kr/hhplus/be/server/infra/QueueTokenRepositoryImpl.java
+++ src/main/java/kr/hhplus/be/server/infra/QueueTokenRepositoryImpl.java
@@ -1,9 +1,41 @@
 package kr.hhplus.be.server.infra;
 
+import java.util.UUID;
+import kr.hhplus.be.server.domain.queue.QueueToken;
 import kr.hhplus.be.server.domain.queue.QueueTokenRepository;
+import kr.hhplus.be.server.domain.user.UserId;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class QueueTokenRepositoryImpl implements QueueTokenRepository {
 
+  @Override
+  public boolean existsValidToken(UserId userId) {
+    return false;
+  }
+
+  @Override
+  public int countAllValid() {
+    return 0;
+  }
+
+  @Override
+  public QueueToken save(QueueToken token) {
+    return null;
+  }
+
+  @Override
+  public QueueToken findValidByUserId(UserId userId) {
+    return null;
+  }
+
+  @Override
+  public QueueToken findById(UUID tokenId) {
+    return null;
+  }
+
+  @Override
+  public int getCurrentServedPosition() {
+    return 0;
+  }
 }
