98182d64dd0a0b34686d3f5ce26df4115bda23a1
OZIIJIN <dpwlsdlqma6@gmail.com>
2025-04-20 22:14:30 +0900

    #9 refactor : QueueManager 제거 후, QueueService 로 통합

--- src/main/java/kr/hhplus/be/server/application/QueueTokenService.java
+++ src/main/java/kr/hhplus/be/server/application/QueueTokenService.java
@@ -1,55 +1,79 @@
 package kr.hhplus.be.server.application;
 
+import java.time.Duration;
 import java.util.UUID;
-import kr.hhplus.be.server.domain.queue.QueueManager;
 import kr.hhplus.be.server.domain.queue.QueueToken;
+import kr.hhplus.be.server.domain.queue.QueueTokenRepository;
 import kr.hhplus.be.server.domain.user.UserId;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class QueueTokenService {
+  private final QueueTokenRepository queueTokenRepository;
+  private static final Duration TTL = Duration.ofMinutes(10);
 
-  private final QueueManager queueManager;
-
-  public QueueTokenService(QueueManager queueManager) {
-    this.queueManager = queueManager;
+  public QueueTokenService(QueueTokenRepository queueTokenRepository) {
+    this.queueTokenRepository = queueTokenRepository;
   }
 
+  @Transactional
   public UUID issueToken(UserId userId) {
-    return queueManager.issue(userId).getId();
+    if (queueTokenRepository.existsValidToken(userId)) {
+      throw new IllegalArgumentException("이미 대기열에 진입하셨습니다.");
+    }
+    int position = queueTokenRepository.countAllValid() + 1;
+    QueueToken token = QueueToken.issue(userId, position, TTL);
+    return queueTokenRepository.save(token).getId();
   }
 
+  @Transactional(readOnly = true)
   public void validate(UUID tokenId) {
-    queueManager.validateToken(tokenId);
+    QueueToken token = queueTokenRepository.findById(tokenId);
+    if (token.isExpired()) throw new IllegalArgumentException("만료된 토큰 입니다.");
+
+    int current = getCurrentPosition();
+    if (!token.isAccessible(current)) throw new IllegalArgumentException("접근 불가");
   }
 
+  @Transactional(readOnly = true)
   public QueueTokenInfo.QueueTokenDetail getMyPosition(UserId userId) {
     int currentPosition = getCurrentPosition();
-    int userPosition = queueManager.getValidToken(userId).getPosition();
+    int userPosition = getValidToken(userId).getPosition();
     return new QueueTokenInfo.QueueTokenDetail(userPosition, currentPosition);
   }
 
+  @Transactional
   public void enterNext() {
-    queueManager.enterNext();
+    int currentPosition = getCurrentPosition();
+
+    QueueToken next = queueTokenRepository.findFirstWaitingAfter(currentPosition);
+
+    if(!next.isExpired()) {
+      next.toEntered();
+    }
   }
 
+  @Transactional
   public void leave(UUID tokenId) {
-    queueManager.exit(tokenId);
-  }
-
-  public int getCurrentPosition() {
-    return queueManager.getCurrentPosition();
-  }
-
-  public QueueToken getValidToken(UserId userId) {
-    return queueManager.getValidToken(userId);
-  }
-
-  public void exitAfterPayment(UserId userId) {
-    QueueToken token = getValidToken(userId);
-    validate(token.getId());
-
+    QueueToken token = queueTokenRepository.findById(tokenId);
     token.toExited();
   }
 
+  @Transactional(readOnly = true)
+  public int getCurrentPosition() {
+    return queueTokenRepository.getCurrentServedPosition() + 1;
+  }
+
+  @Transactional(readOnly = true)
+  public QueueToken getValidToken(UserId userId) {
+    return queueTokenRepository.findValidByUserId(userId);
+  }
+
+  @Transactional
+  public void exitAfterPayment(UserId userId) {
+    QueueToken token = getValidToken(userId);
+    validate(token.getId());
+    token.toExited();
+  }
 }
